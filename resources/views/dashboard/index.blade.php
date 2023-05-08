8942410fb408a4f44c460d1a4a57f166075453d3
FaresAriandha <AkunKoding2009@gmail.com>
2023-05-08 19:12:41 +0700

    test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-  <h1 class="h2">Welcome back, "test"</h1>
+  <h1 class="h2">Welcome back, {{ auth()->user()->name }}</h1>
 </div>
 <div class="row">
   <div class="col-lg-6">
